cffbba2bb2806b061a458c495e9c358ef4c6f997
CARMEN QUESADA <carmenquesadarguez@gmail.com>
2025-07-03 18:57:58 +0200

    migrations

--- DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
+++ DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
@@ -6,9 +6,89 @@ module.exports = {
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
-      }
+      },
       // TODO: Include the rest of the fields of the Restaurants table
-
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      address: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      postalCode: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      url: {
+        type: Sequelize.STRING
+      },
+      shippingCosts: {
+        allowNull: false,
+        defaultValue: 0.0,
+        type: Sequelize.DOUBLE
+      },
+      averageServiceMinutes: {
+        allowNull: true,
+        type: Sequelize.DOUBLE
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      phone: {
+        type: Sequelize.STRING
+      },
+      logo: {
+        type: Sequelize.STRING
+      },
+      heroImage: {
+        type: Sequelize.STRING
+      },
+      status: {
+        allowNull: false,
+        type: Sequelize.ENUM,
+        values: [
+          'online',
+          'offline',
+          'closed',
+          'temporarily closed'
+        ],
+        defaultValue: 'offline'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      },
+      restaurantCategoryId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'RestaurantCategories'
+          },
+          key: 'id'
+        }
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: {
+            tableName: 'users'
+          },
+          key: 'id'
+        }
+      }
     })
   },
   down: async (queryInterface, Sequelize) => {
